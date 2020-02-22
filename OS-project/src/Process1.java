7e39996bad34c0ef3a6aff7b26e64bc688d6397d
Reem Salah <47977185+reeemsalah@users.noreply.github.com>
2020-02-22 17:42:35 +0200

    Second Commit - Part 1

--- OS-project/src/Process1.java
+++ OS-project/src/Process1.java
@@ -1,11 +1,34 @@
 
+import java.io.*;
 public class Process1 implements Runnable
 {
 
+	private Object fileName;
+	public Process1 (Object fileName)
+	{
+		this.fileName=fileName;
+	}
+	
 	@Override
-	public void run() {
+	public void run()
+	{
 		// TODO Auto-generated method stub
+		try
+		{
+		String fileNameString="D:\\"+fileName.toString()+".txt";
+		File file = new File(fileNameString); 
+		  
+		  BufferedReader br = new BufferedReader(new FileReader(file)); 
+		  
+		  String st; 
+		  while ((st = br.readLine()) != null) 
+		    System.out.println(st);
 		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 }
